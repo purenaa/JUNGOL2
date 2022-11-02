2831e0f0239c7747b04e2b1405ca7a181a67b710
purenaa <jyn8313@gmail.com>
2022-11-02 16:53:16 +0900

    초기 커밋.

---
+++ 1102/B2857.cpp
@@ -0,0 +1,26 @@
+//JUNGOL_BIGINNER_2857 : 세로읽기
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	char toy[5][20] = { NULL };
+	int i, j;
+
+	for (j = 0; j < 5; j++)
+	{
+		scanf("%s", &toy[j]);
+	}
+
+	for (i = 0; i < 15; i++) {
+		for (j = 0; j < 5; j++) {
+			if (toy[j][i] == NULL)
+				continue;
+			else
+				printf("%c", toy[j][i]);
+			}
+		}
+	return 0;
+}
